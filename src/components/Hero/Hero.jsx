b47c596ba0dfd0abe95c1a24c4d72f8265646792
Nancy-007 <gurdevnancy83@gmail.com>
2025-05-08 18:24:38 +0530

    hero changes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
   return (
     <div className='hero container'>
     <div className="hero-text">
-    <h1>We Ensur better education fir a better world</h1>
+    <h1>We Ensure better education fir a better world</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut aperiam laboriosam, placeat harum dolorum velit, iusto repellat nobis rerum quae quaerat ratione qui incidunt eveniet ex, suscipit dolorem voluptates tempore!</p>
     <button className='btn'>Explore more <img src={dark_arrow} alt="" /></button>
     </div>
